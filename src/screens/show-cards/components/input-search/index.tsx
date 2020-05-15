cbb454e2dae5a63b1724e9818a9572c8fc226a35
João Damazio <joaohd98@gmail.com>
2020-05-15 12:44:09 -0300

    Add font awesome

--- src/screens/show-cards/components/input-search/index.tsx
+++ src/screens/show-cards/components/input-search/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {ShowCardsInputSearchStyles} from "./styles";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export class ShowCardsInputSearch extends React.PureComponent {
   render() {
@@ -8,6 +9,7 @@ export class ShowCardsInputSearch extends React.PureComponent {
     return (
       <View>
         <Input />
+        <Icon name="rocket" size={30} color="#900" />
         <EraseButton />
         <CancelButton />
       </View>
