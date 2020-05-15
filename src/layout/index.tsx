e46e32744988bf6ae84327c934fabff2c05de162
João Damazio <joaohd98@gmail.com>
2020-05-15 11:20:50 -0300

    Add navigation

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import {ShowCardsScreen} from '../screens/show-cards';
+
+const Stack = createStackNavigator();
 
 export class Layout extends React.PureComponent {
 
   render() {
     return (
-      <ShowCardsScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={ShowCardsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 
