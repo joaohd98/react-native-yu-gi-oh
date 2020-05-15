c07d463f2d2f4cd44c99f23a22f7e3dde2eda779
João Damazio <joaohd98@gmail.com>
2020-05-15 11:07:05 -0300

    Add basic layout

---
+++ src/layout/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ShowCardsScreen} from '../screens/show-cards';
+
+export class Layout extends React.PureComponent {
+
+  render() {
+    return (
+      <ShowCardsScreen />
+    );
+  }
+
+}
---
+++ src/screens/show-cards/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export class ShowCardsScreen extends React.PureComponent {
+
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <Text>ShowCardsScreen</Text>
+      </View>
+    );
+  }
+
+}
